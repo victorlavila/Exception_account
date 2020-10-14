393f64f540865b58d8a6da8fae8e28e662df5330
Victor Leonardo <victorlavila@gmail.com>
2020-10-14 12:15:19 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.Exceptions;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Account date: ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+		}
+		catch(Exceptions e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		
+
+	}
+
+}
---
+++ src/model/exceptions/Exceptions.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class Exceptions extends RuntimeException {
+	
+	private static final long serialVersionUID = 1l;
+	
+	public Exceptions(String msg) {
+		super(msg);
+	}
+
+}
